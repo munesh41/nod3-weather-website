1857cf63f1384e68adcb1f87acbded320777ff22
munesh41 <m.mnesh@gmail.com>
2019-04-16 03:02:44 +0300

    Weather format rctification

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,13 +7,13 @@ const forecast = (latitude,longitude,callback)=>{
         }else if (body.error){
             callback('Please enter a valid coodinate')
         }else{
-            callback(undefined,{
-                latitude:body.latitude,
-                longitude:body.longitude,
-                timezone:body.timezone,
-                weather:body.currently.summary,
-                temp:body.currently.temperature
-            })
+            callback(undefined,callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+                // latitude:body.latitude,
+                // longitude:body.longitude,
+                // timezone:body.timezone,
+                // weather:body.currently.summary,
+                // temp:body.currently.temperature
+            )
         }
     })
 }
